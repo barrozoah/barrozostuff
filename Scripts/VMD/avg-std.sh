8789621fb4d42e86efea24ac14679c7f88ed63c5
Alexandre H. Barrozo <barrozo.ah@gmail.com>
2015-05-06 16:22:03 +0200

    Modified script for averages used with VMD DCD analysis

--- Scripts/VMD/avg-std.sh
+++ Scripts/VMD/avg-std.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-awk '{sum += $2; sumsq += ($2)^2} END {print sum/NR, sqrt((sumsq-sum^2/NR)/NR)}' $1
+#sed 's/\./,/g' $1 > tmp.log
+#mv tmp.log $1
+awk 'NR > 1  {sum += $2; sumsq += ($2)^2} END {print NR-1, sum/(NR-1), sqrt((sumsq-sum^2/(NR-1))/(NR-1))}' $1
